8976cec032d3e3296ce04e913c1a238b7ca0ce92
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-08-16 18:24:32 +0530

    refactor file structure

--- src/main.zig
+++ src/main.zig
@@ -1,150 +1,126 @@
 const std = @import("std");
 const vaxis = @import("vaxis");
 const vxfw = vaxis.vxfw;
+const screen = @import("interfaces/screen.zig");
+const TextInput = vaxis.widgets.TextInput;
+const Cell = vaxis.Cell;
+const border = vaxis.widgets.border;
 
-const Text = struct {
-    text: []u8,
-    col: i17,
-    row: i17,
-
-    pub fn init(text_data: []u8, col: i17) Text {
-        return .{
-            .text = text_data,
-            .col = col,
-            .row = 5,
-        };
-    }
-
-    pub fn move_up(self: *Text) void {
-        if (self.row - 1 < 0) {
-            return;
-        }
-        self.row -|= 1;
-    }
-
-    pub fn move_down(self: *Text) void {
-        if (self.row + 1 > 50) {
-            return;
-        }
-        self.row +|= 1;
-    }
-};
-
-const Model = struct {
-    counter: i32,
-    idx: usize = 0,
-    texts: std.ArrayList([]u8),
-    text_texts: std.ArrayList(Text),
-    allocator: std.mem.Allocator,
-
-    pub fn widget(self: *Model) vxfw.Widget {
-        return .{ .userdata = self, .eventHandler = Model.eventHandler, .drawFn = Model.drawFn };
-    }
-
-    pub fn eventHandler(userdata: *anyopaque, ctx: *vxfw.EventContext, event: vxfw.Event) anyerror!void {
-        const self: *Model = @ptrCast(@alignCast(userdata));
-
-        switch (event) {
-            .key_press => |key| {
-                if (key.matches('c', .{ .ctrl = true }) or key.matches('q', .{})) {
-                    ctx.quit = true;
-                    return;
-                } else if (key.matches('h', .{})) {
-                    const val: i32 = @intCast(self.idx);
-                    if (val - 1 >= 0) {
-                        self.idx -|= 1;
-                    }
-                } else if (key.matches('l', .{})) {
-                    if (self.idx + 1 < self.text_texts.items.len) {
-                        self.idx +|= 1;
-                    }
-                } else if (key.matches('j', .{})) {
-                    self.text_texts.items[self.idx].move_down();
-                } else if (key.matches('k', .{})) {
-                    self.text_texts.items[self.idx].move_up();
-                } else if (key.matches('e', .{})) {
-                    var col: i17 = 0;
-                    const offset: i17 = 3;
-                    for (self.text_texts.items) |i| {
-                        col +|= @intCast(i.text.len);
-                        col +|= offset;
-                    }
-                    try self.text_texts.append(Text.init(try std.fmt.allocPrint(self.allocator, "This is {d} line", .{self.counter}), col));
-                }
-            },
-            else => {},
-        }
-
-        ctx.consumeAndRedraw();
-    }
-
-    pub fn drawFn(userdata: *anyopaque, ctx: vxfw.DrawContext) std.mem.Allocator.Error!vxfw.Surface {
-        const self: *Model = @ptrCast(@alignCast(userdata));
-
-        const text = try std.fmt.allocPrint(ctx.arena, "current index : {d}", .{self.idx});
-        const text_w: vxfw.Text = .{ .text = text };
-
-        const text_subSurface: vxfw.SubSurface = .{ .origin = .{ .col = 0, .row = 0 }, .surface = try text_w.draw(ctx) };
-        const childrens = try ctx.arena.alloc(vxfw.SubSurface, 1 + self.text_texts.items.len);
-
-        childrens[0] = text_subSurface;
-
-        var idx: usize = 1;
-
-        for (self.text_texts.items) |i| {
-            var tt: vxfw.Text = undefined;
-            const folder = "\u{1F4C1}";
-            if (idx == self.idx + 1) {
-                const aa = try std.fmt.allocPrint(ctx.arena, "{s} {s}", .{folder, i.text});
-                tt = .{ .text = aa, .style = .{ .bold = true, .bg = .{ .rgb = [3]u8 {40, 50, 20}}, .ul_style =  .single} };
-            } else {
-                tt = .{ .text = i.text };
-            }
-            var col: i17= i.col;
-            if (idx > self.idx + 1) {
-                col = i.col + @as(i17, folder.len);
-            } else {
-                col = i.col;
-            }
-            const t: vxfw.SubSurface = .{ .origin = .{ .col = col, .row = i.row }, .surface = try tt.draw(ctx) };
-            childrens[idx] = t;
-            idx +|= 1;
-        }
-
-        return .{ .size = ctx.max.size(), .widget = self.widget(), .buffer = &.{}, .children = childrens };
-    }
+const Event = union(enum) {
+    key_press: vaxis.Key,
+    winsize: vaxis.Winsize,
+    focus_in,
+    foo: u8,
 };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-
     const allocator = gpa.allocator();
 
     var app = try vxfw.App.init(allocator);
     defer app.deinit();
 
-    const model = try allocator.create(Model);
-    defer {
-        for (model.texts.items) |i| {
-            allocator.free(i);
-        }
+    var tty = try vaxis.Tty.init();
+    defer tty.deinit();
 
-        for (model.text_texts.items) |i| {
-            allocator.free(i.text);
-        }
-        model.texts.deinit();
-        model.text_texts.deinit();
-        allocator.destroy(model);
-    }
+    var vx = try vaxis.init(allocator, .{});
+    defer vx.deinit(allocator, tty.anyWriter());
 
-    model.* = .{
-        .counter = 10,
-        .texts = std.ArrayList([]u8).init(allocator),
-        .text_texts = std.ArrayList(Text).init(allocator),
-        .allocator = allocator,
+    var loop: vaxis.Loop(Event) = .{
+        .tty = &tty,
+        .vaxis = &vx,
     };
 
-    try app.run(model.widget(), .{});
-    std.debug.print("asfasf", .{});
+    try loop.init();
+    try loop.start();
+    defer loop.stop();
+
+    try vx.enterAltScreen(tty.anyWriter());
+
+    var color_idx: u8 = 255;
+    var text_input = TextInput.init(allocator, &vx.unicode);
+    defer text_input.deinit();
+
+    try vx.queryTerminal(tty.anyWriter(), 1 * std.time.ns_per_s);
+
+    while (true) {
+        const event = loop.nextEvent();
+        switch (event) {
+            .key_press => |key| {
+                color_idx = switch (color_idx) {
+                    255 => 0,
+                    else => color_idx + 1,
+                };
+                if (key.matches('c', .{ .ctrl = true }) or key.matches('q', .{})) {
+                    break;
+                } else if (key.matches('l', .{ .ctrl = true })) {
+                    vx.queueRefresh();
+                } else {
+                    try text_input.update(.{ .key_press = key });
+                }
+            },
+            .winsize => |ws| try vx.resize(allocator, tty.anyWriter(), ws),
+            else => {},
+        }
+
+        const win = vx.window();
+        win.clear();
+
+        var screen_instance = screen.Screen_{
+            .root_dir_name = "/home/hexa/",
+            .curr_dir_name = "",
+            .child_dir_name = "",
+        };
+
+        const screen_sizes = screen_instance.getScreenSizes(win);
+
+        const size = screen_instance.getRootDirSize();
+
+        const left_pane = screen_instance.createLeftPanel(win, screen_sizes);
+        const middle_pane = screen_instance.createMiddlePanel(win, screen_sizes);
+        const right_pane = screen_instance.createRightPanel(win, screen_sizes);
+
+        var buff: [16]u8 = undefined;
+        const file_size = try std.fmt.bufPrint(&buff, "{!}", .{size});
+        std.debug.print("file size: {s}\n", .{file_size});
+
+        const get_files = try screen_instance.getRootDirFolder(allocator);
+        defer {
+            for (get_files) |file| {
+                allocator.free(file);
+            }
+            allocator.free(get_files);
+        }
+
+        _ = left_pane.printSegment(.{ .text = "Root Folder" }, .{ .row_offset = 1, .col_offset = 1 });
+        _ = left_pane.printSegment(.{ .text = file_size }, .{ .row_offset = 2, .col_offset = 1 });
+
+        var i: u8 = 3;
+        for (get_files) |file| {
+            _ = left_pane.printSegment(.{ .text = file }, .{ .row_offset = i, .col_offset = 1 });
+            i += 1;
+        }
+
+        _ = middle_pane.printSegment(.{ .text = "Curr Folder" }, .{ .row_offset = 1, .col_offset = 1 });
+        _ = right_pane.printSegment(.{ .text = "Child Folder" }, .{ .row_offset = 1, .col_offset = 1 });
+
+        const text = win.child(.{
+            .x_off = win.width / 2 - 20,
+            .y_off = win.height / 2 - 5,
+            .width = 40,
+            .height = 20,
+            .border = .{
+                .where = .all,
+            },
+        });
+
+        _ = text.printSegment(.{ .text = "Find files" }, .{ .row_offset = 3, .col_offset = 10 });
+
+        text_input.draw(text);
+
+        try vx.render(tty.anyWriter());
+    }
+
+    std.debug.print("Application finished\n", .{});
 }
