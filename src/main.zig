923c718ba64172db983b4aa55072980cb1ee0557
Prashantha S P <prashanth.sp98@gmail.com>
2025-08-16 17:52:40 +0530

    Handling own event loop and window

--- src/main.zig
+++ src/main.zig
@@ -2,149 +2,77 @@ const std = @import("std");
 const vaxis = @import("vaxis");
 const vxfw = vaxis.vxfw;
 
-const Text = struct {
-    text: []u8,
-    col: i17,
-    row: i17,
-
-    pub fn init(text_data: []u8, col: i17) Text {
-        return .{
-            .text = text_data,
-            .col = col,
-            .row = 5,
-        };
-    }
-
-    pub fn move_up(self: *Text) void {
-        if (self.row - 1 < 0) {
-            return;
-        }
-        self.row -|= 1;
-    }
-
-    pub fn move_down(self: *Text) void {
-        if (self.row + 1 > 50) {
-            return;
-        }
-        self.row +|= 1;
-    }
-};
-
-const Model = struct {
-    counter: i32,
-    idx: usize = 0,
-    texts: std.ArrayList([]u8),
-    text_texts: std.ArrayList(Text),
-    allocator: std.mem.Allocator,
-
-    pub fn widget(self: *Model) vxfw.Widget {
-        return .{ .userdata = self, .eventHandler = Model.eventHandler, .drawFn = Model.drawFn };
-    }
-
-    pub fn eventHandler(userdata: *anyopaque, ctx: *vxfw.EventContext, event: vxfw.Event) anyerror!void {
-        const self: *Model = @ptrCast(@alignCast(userdata));
-
-        switch (event) {
-            .key_press => |key| {
-                if (key.matches('c', .{ .ctrl = true }) or key.matches('q', .{})) {
-                    ctx.quit = true;
-                    return;
-                } else if (key.matches('h', .{})) {
-                    const val: i32 = @intCast(self.idx);
-                    if (val - 1 >= 0) {
-                        self.idx -|= 1;
-                    }
-                } else if (key.matches('l', .{})) {
-                    if (self.idx + 1 < self.text_texts.items.len) {
-                        self.idx +|= 1;
-                    }
-                } else if (key.matches('j', .{})) {
-                    self.text_texts.items[self.idx].move_down();
-                } else if (key.matches('k', .{})) {
-                    self.text_texts.items[self.idx].move_up();
-                } else if (key.matches('e', .{})) {
-                    var col: i17 = 0;
-                    const offset: i17 = 3;
-                    for (self.text_texts.items) |i| {
-                        col +|= @intCast(i.text.len);
-                        col +|= offset;
-                    }
-                    try self.text_texts.append(Text.init(try std.fmt.allocPrint(self.allocator, "This is {d} line", .{self.counter}), col));
-                }
-            },
-            else => {},
-        }
-
-        ctx.consumeAndRedraw();
-    }
-
-    pub fn drawFn(userdata: *anyopaque, ctx: vxfw.DrawContext) std.mem.Allocator.Error!vxfw.Surface {
-        const self: *Model = @ptrCast(@alignCast(userdata));
-
-        const text = try std.fmt.allocPrint(ctx.arena, "current index : {d}", .{self.idx});
-        const text_w: vxfw.Text = .{ .text = text };
-
-        const text_subSurface: vxfw.SubSurface = .{ .origin = .{ .col = 0, .row = 0 }, .surface = try text_w.draw(ctx) };
-        const childrens = try ctx.arena.alloc(vxfw.SubSurface, 1 + self.text_texts.items.len);
-
-        childrens[0] = text_subSurface;
-
-        var idx: usize = 1;
-
-        for (self.text_texts.items) |i| {
-            var tt: vxfw.Text = undefined;
-            const folder = "\u{1F4C1}";
-            if (idx == self.idx + 1) {
-                const aa = try std.fmt.allocPrint(ctx.arena, "{s} {s}", .{folder, i.text});
-                tt = .{ .text = aa, .style = .{ .bold = true, .bg = .{ .rgb = [3]u8 {40, 50, 20}}, .ul_style =  .single} };
-            } else {
-                tt = .{ .text = i.text };
-            }
-            var col: i17= i.col;
-            if (idx > self.idx + 1) {
-                col = i.col + @as(i17, folder.len);
-            } else {
-                col = i.col;
-            }
-            const t: vxfw.SubSurface = .{ .origin = .{ .col = col, .row = i.row }, .surface = try tt.draw(ctx) };
-            childrens[idx] = t;
-            idx +|= 1;
-        }
-
-        return .{ .size = ctx.max.size(), .widget = self.widget(), .buffer = &.{}, .children = childrens };
-    }
+const EventLoop = union(enum) {
+    key_press: vaxis.Key,
+    winsize: vaxis.Winsize,
+    focus_in,
 };
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    const allocator = gpa.allocator();
+    var tty = try vaxis.Tty.init();
+    defer tty.deinit();
 
-    var app = try vxfw.App.init(allocator);
-    defer app.deinit();
+    var vx = try vaxis.init(allocator, .{});
+    defer vx.deinit(allocator, tty.anyWriter());
 
-    const model = try allocator.create(Model);
-    defer {
-        for (model.texts.items) |i| {
-            allocator.free(i);
-        }
-
-        for (model.text_texts.items) |i| {
-            allocator.free(i.text);
-        }
-        model.texts.deinit();
-        model.text_texts.deinit();
-        allocator.destroy(model);
-    }
-
-    model.* = .{
-        .counter = 10,
-        .texts = std.ArrayList([]u8).init(allocator),
-        .text_texts = std.ArrayList(Text).init(allocator),
-        .allocator = allocator,
+    var loop: vaxis.Loop(EventLoop) = .{
+        .tty = &tty,
+        .vaxis = &vx,
     };
 
-    try app.run(model.widget(), .{});
-    std.debug.print("asfasf", .{});
+    try loop.init();
+    try loop.start();
+    defer loop.stop();
+
+    try vx.enterAltScreen(tty.anyWriter());
+
+    try vx.queryTerminal(tty.anyWriter(), 1 * std.time.ns_per_s);
+
+    var quit = false;
+    var color_id: u8 = 0;
+
+    var text_1 = vaxis.widgets.TextInput.init(allocator, &vx.unicode);
+    defer text_1.deinit();
+
+    while (!quit) {
+        const event = loop.nextEvent();
+        switch (event) {
+            .key_press => |key| {
+                color_id = switch (color_id) {
+                    255 => 0,
+                    else => color_id + 1,
+                };
+
+                if (key.matches('q', .{})) {
+                    quit = true;
+                } else {
+                    try text_1.update(.{ .key_press = key });
+                }
+            },
+            .winsize => |ws| try vx.resize(allocator, tty.anyWriter(), ws),
+            else => {},
+        }
+
+        const window = vx.window();
+        window.clear();
+
+        const style: vaxis.Style = .{
+            .fg = .{ .index = color_id },
+        };
+
+        const child_1 = window.child(.{
+            .border = .{ .style = style, .where = .all },
+            .width = window.width,
+            .height = window.height,
+            .x_off = 1,
+            .y_off = 1,
+        });
+
+        text_1.draw(child_1);
+        try vx.render(tty.anyWriter());
+    }
 }
