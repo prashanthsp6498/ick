7ce61d6e00f743ace775530f28087015d10b2c11
Prashantha S P <prashanth.sp98@gmail.com>
2025-08-17 11:23:51 +0530

    Added `-Dversion-string` in build option and it's result in `exe_options`

--- build.zig
+++ build.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+var version: std.SemanticVersion = .{ .major = 0, .minor = 0, .patch = 0, .build = "debug"};
+
 const BuildParams = struct {
     target: std.Build.ResolvedTarget,
     optimize: std.builtin.OptimizeMode,
@@ -16,15 +18,27 @@ const BuildParams = struct {
     }
 };
 
+fn get_version(b: *std.Build) std.SemanticVersion {
+    const version_string = b.option([]const u8, "version-string", "Set the version of the build. Must be semantic");
+    if (version_string) |semver_string| {
+        return std.SemanticVersion.parse(semver_string) catch |err| {
+            std.debug.panic("Expected -Dversion-string={s} to be a semantic version: {}", .{ semver_string, err });
+        };
+    }
+
+    return version; // for now later change it to git tag
+}
+
 fn get_exe(params: BuildParams) struct { *std.Build.Module, *std.Build.Step.Compile } {
     const target = params.target;
     const optimize = params.optimize;
     const b = params.b;
-
     const exe_options = blk: {
         const exe_options = b.addOptions();
         exe_options.step.name = "ick exe options";
         exe_options.addOption(bool, "debug_build", optimize == .Debug);
+        exe_options.addOption(std.SemanticVersion, "version", version);
+        exe_options.addOption([]const u8, "version_string", b.fmt("{}", .{version}));
 
         break :blk exe_options.createModule();
     };
@@ -64,13 +78,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const build_params = BuildParams {
+    const build_params = BuildParams{
         .b = b,
         .optimize = optimize,
         .target = target,
         .exe_name = "ick",
     };
 
+    version = get_version(b);
     const module = get_exe(build_params);
     b.installArtifact(module.@"1");
 
