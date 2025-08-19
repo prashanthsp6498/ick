c6853a0b8695a5208091d744fae9c55790d40f58
Hemanthsp999 <hemanth.sp03@gmail.com>
2025-08-19 22:38:07 +0530

    add file

--- src/core/core.zig
+++ src/core/core.zig
@@ -4,129 +4,41 @@ const vxfw = vaxis.vxfw;
 const cwd = std.fs.cwd();
 const logger = @import("logger").Logger;
 
-const folderStructure = struct {
+pub const folderStructure = struct {
     curr: [][]const u8,
-
     parent: ?*folderStructure,
     child: ?*folderStructure,
+    allocator: std.mem.Allocator,
 };
 
 pub const File = struct {
-    root_path: []const u8 = ".",
-    curr_path: []u8 = "",
-    child_path: []u8 = "",
+    path: []const u8 = "/home/hexa",
     allocator: std.mem.Allocator,
 
-    pub const handler = struct {
-        getRootFile: [][]const u8,
-        getCurrFile: [][]const u8,
-        getChildFile: [][]const u8,
-        folderSize: u8,
-        allocator: std.mem.Allocator,
-
-        pub fn deinit(self: *handler) void {
-            for (self.getRootFile) |file| {
-                self.allocator.free(file);
-            }
-
-            self.allocator.free(self.getRootFile);
-            self.allocator.free(self.getCurrFile);
-            self.allocator.free(self.getChildFile);
-        }
-    };
-
-    pub fn init(allocator: std.mem.Allocator) File {
+    pub fn init(alloc: std.mem.Allocator) File {
         return .{
-            .allocator = allocator,
+            .allocator = alloc,
         };
     }
-
-    pub fn getContent(self: *File) !handler {
-        return .{
-            .getRootFile = try self.getRootDir(),
-            .getCurrFile = try self.getCurrDir(),
-            .getChildFile = try self.getChildDir(),
-            .folderSize = try self.getFolderSize(),
-            .allocator = self.allocator,
-        };
-    }
-
-    fn getFolderSize(self: *const File) !u8 {
-        var dir = try std.fs.cwd().openDir(self.root_path, .{ .iterate = true });
-        defer dir.close();
-
-        var total_size: u8 = 0;
-
-        var it = dir.iterate();
-        while (try it.next()) |entry| {
-            _ = entry.name;
-            total_size += 1;
+    pub fn deinit(self: *folderStructure) void {
+        for (self.curr) |file| {
+            self.allocator.free(file);
         }
-
-        return total_size;
+        self.allocator.free(self.curr);
     }
 
-    fn getRootDir(self: *const File) ![][]const u8 {
-        var dir = try std.fs.cwd().openDir(self.root_path, .{ .iterate = true });
-        defer dir.close();
+    pub fn currDir(self: *const File) !folderStructure {
+        var open = try std.fs.openDirAbsolute(self.path, .{ .iterate = true });
+        defer open.close();
+        var it = open.iterate();
 
-        var files = std.ArrayList([]const u8).init(self.allocator);
-        defer files.deinit();
-
-        var it = dir.iterate();
+        var folders = std.ArrayList([]const u8).init(self.allocator);
 
         while (try it.next()) |entry| {
             const file = try self.allocator.dupe(u8, entry.name);
-            try files.append(file);
+            try folders.append(file);
         }
 
-        return files.toOwnedSlice();
-    }
-
-    fn getCurrDir(self: *const File) ![][]const u8 {
-        var dir = try std.fs.cwd().openDir(self.root_path, .{ .iterate = true });
-        defer dir.close();
-
-        var files = std.ArrayList([]const u8).init(self.allocator);
-        defer files.deinit();
-
-        var it = dir.iterate();
-        while (try it.next()) |entry| {
-            try files.append(entry.name);
-        }
-
-        return files.toOwnedSlice();
-    }
-
-    fn getChildDir(self: *const File) ![][]const u8 {
-        var dir = try std.fs.cwd().openDir(self.root_path, .{ .iterate = true });
-        defer dir.close();
-
-        var files = std.ArrayList([]const u8).init(self.allocator);
-        defer files.deinit();
-
-        var it = dir.iterate();
-        while (try it.next()) |entry| {
-            try files.append(entry.name);
-        }
-
-        return files.toOwnedSlice();
+        return folderStructure{ .curr = try folders.toOwnedSlice(), .parent = undefined, .child = undefined, .allocator = self.allocator };
     }
 };
-
-//
-test "file test" {
-    var gap = std.heap.DebugAllocator(.{}){};
-    defer _ = gap.deinit();
-    const allocator = gap.allocator();
-    var file = File.init(allocator);
-    const contents = file.getContent();
-    defer contents.deinit();
-    //const size = try contents.folderSize;
-
-    const get_files = try contents.getRootFile;
-    for (get_files) |get_file| {
-        std.debug.print("{s}", .{get_file});
-    }
-    //std.debug.print("{s}", .{size});
-}
